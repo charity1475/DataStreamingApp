4861d0f677cd4dc8e3cf219382b8f0cff3aa20f3
Steinny <charitymbisi@gmail.com>
2020-10-15 21:50:56 +0300

    producer

--- kafka_producer.py
+++ kafka_producer.py
@@ -18,5 +18,14 @@ if __name__ == "__main__":
             if stream_api_response.status_code == 200:
                 for api_response_message in stream_api_response.iter_lines:
                     print('message received...')
+                    print(api_response_message)
+                    print(type(api_response_message))
+
+                    api_response_message = json.loads(api_response_message)
+                    print('message to be sent...')
+                    print(api_response_message)
+                    print(type(api_response_message))
+                    kafka_producer_obj.send(kafka_topic_name,api_response_message)
                     
+
 
